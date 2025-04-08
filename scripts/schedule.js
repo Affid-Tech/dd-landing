dc463ab5dfb5df0beb3e77de3ea6eae1ca5d58af
Affid <47119594+Affid@users.noreply.github.com>
2025-04-08 22:32:28 +0200

    fix card

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -3,45 +3,43 @@ window.loadNextGame = async function () {
     if (!container) return;
   
     try {
-      const response = await fetch("https://raw.githubusercontent.com/Affid-Tech/dnd-schedule/refs/heads/main/data/games.json");
-      const games = await response.json();
-  
-      const now = new Date();
-      const futureGames = games
+        const response = await fetch("https://raw.githubusercontent.com/Affid-Tech/dnd-schedule/refs/heads/main/data/games.json");
+        const games = await response.json();
+
+        const now = new Date();
+        const futureGames = games
         .filter(game => {
-          const date = new Date(game.date);
-          return !isNaN(date) && date > now;
+            const date = new Date(game.date);
+            return !isNaN(date) && date > now;
         })
         .sort((a, b) => new Date(a.date) - new Date(b.date));
-  
-      if (futureGames.length === 0) {
+
+        if (futureGames.length === 0) {
         container.style.display = "none";
         return;
-      }
-  
-      const nextGame = futureGames[0];
-      const date = new Date(nextGame.date);
-  
-      container.innerHTML = `
-        <div class="game-card">
-            <div class="game-card-header">
-            🎲 <strong>${nextGame.title}</strong>
-            </div>
-            <div class="game-card-content">
+        }
+
+        const nextGame = futureGames[0];
+        const formattedDate = new Date(nextGame.date).toLocaleDateString('ru-RU', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+
+        container.innerHTML = `
+        <sl-card class="game-card" style="margin-top: 1rem;">
+            <strong slot="header">🎲 ${nextGame.title}</strong>
+            <div>
             <p>🧙 Мастер: <strong>${nextGame.dm}</strong></p>
-            <p>🗓 Время: <strong>${date.toLocaleDateString('ru-RU', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-            })}</strong></p>
+            <p>🗓 Время: <strong>${formattedDate}</strong></p>
             <p>👥 Игроков: <strong>${nextGame.currentPlayers} / ${nextGame.maxPlayers}</strong></p>
             <p>🕓 Длительность: <strong>${nextGame.duration} ч.</strong></p>
             <p class="game-description">${nextGame.description}</p>
             </div>
-        </div>
+        </sl-card>
         `;
     } catch (error) {
       container.innerHTML = "<p>⚠️ Ошибка загрузки расписания.</p>";
